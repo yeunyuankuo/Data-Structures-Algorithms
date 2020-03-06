2f565eab62ba06a2178c717fa683a4fe7182951d
Jessie Kuo 郭允元 <yeunyuak@usc.edu>
2020-03-05 22:01:40 -0800

    [Added] min stack solution

---
+++ LeetCode/Stack/MinStack.java
@@ -0,0 +1,36 @@
+package Stack;
+
+import java.util.Stack;
+
+/**
+ * All operations will only take O(1) time.
+ * Space complexity as a whole is O(N).
+ */
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.size() == 0) minStack.push(x);
+        else minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
